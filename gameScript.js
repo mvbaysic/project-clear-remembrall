49da674da02e3a82012f3759765949206802e08b
Ember <144258198+EmberCahya@users.noreply.github.com>
2024-07-02 08:46:19 +0800

    Rephrasing

--- gameScript.js
+++ gameScript.js
@@ -27,15 +27,15 @@ window.onload = function() {
     <br/>
     <br/>
     <li>Make pairs of similiar blocks by flipping them.</li>
-    <li>To flip a block you can click on it.</li>
-    <li>If two blocks you clicked are not similar, they will be flipped back.</li>
-    <p style="font-size:18px;">Click one of the following mode to start the game.</p>
+    <li>To flip a block, click or tap on it.</li>
+    <li>If they don't match, both blocks will flip back, faced down.</li>
+    <p style="font-size:18px;">Click one of the following modes to start a game.</p>
     </div>
-    <button onclick="start(3, 4)">3 x 4</button>
+    <button onclick="start(3, 4)">Easy (3 x 4)</button>
     <!--button onclick="start(4, 4)" style="w">4 x 4</button-->
-    <button onclick="start(4, 5)">4 x 5</button>
+    <button onclick="start(4, 5)">Medium (4 x 5)</button>
     <!--button onclick="start(5, 6)">5 x 6</button-->
-    <button onclick="start(6, 6)">6 x 6</button></center>`);
+    <button onclick="start(6, 6)">Hard (6 x 6)</button></center>`);
 }
 
 //Starting the game
@@ -144,14 +144,14 @@ function change(x) {
               time = `${min} minute(s) and ${sec} second(s)`;
           }
           setTimeout(function() {
-              $("#ol").html(`<center><div id="iol"><h2>Congrats!</h2>
+              $("#ol").html(`<center><div id="iol"><h2>Great job!</h2>
                 <p style="font-size:23px;padding:10px;">You completed the ${mode} mode in ${moves} moves. It took you ${time}.</p>
-                <p style="font-size:18px">Comment Your Score!<br/>Play Again ?</p>
-                <button onclick="start(3, 4)">3 x 4</button>
+                <p style="font-size:18px">Let's keep your memory sharp.<br/>Play Again ?</p>
+                <button onclick="start(3, 4)">Easy (3 x 4)</button>
                 <!--button onclick="start(4, 4)" style="w">4 x 4</button-->
-                <button onclick="start(4, 5)">4 x 5</button>
+                <button onclick="start(4, 5)">Medium (4 x 5)</button>
                 <!--button onclick="start(5, 6)">5 x 6</button-->
-                <button onclick="start(6, 6)">6 x 6</button>
+                <button onclick="start(6, 6)">Hard (6 x 6)</button>
                 </div></center>`);
               $("#ol").fadeIn(750);
           }, 1500);
